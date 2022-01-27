8955afc57e541d73d3b47f56f2de1cd600a1f023
Maulana Ardiansyah <maul.ardiansyahh@gmail.com>
2022-01-27 15:28:08 +0700

    Release 0.0.2.

---
+++ Example/Pods/Target Support Files/Pods-Quantum_Example/Pods-Quantum_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Quantum_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Quantum_Example
+@end
---
+++ Example/Pods/Target Support Files/Quantum/Quantum-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Quantum : NSObject
+@end
+@implementation PodsDummy_Quantum
+@end
