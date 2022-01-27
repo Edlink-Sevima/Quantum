87a7d84b8f26b7e2c4c3271d3781587c81d5c133
Maulana Ardiansyah <maul.ardiansyahh@gmail.com>
2022-01-27 14:51:40 +0700

    Initial commit

---
+++ Example/Quantum/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Quantum
+//
+//  Created by mauuulana on 01/27/2022.
+//  Copyright (c) 2022 mauuulana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
